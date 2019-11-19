54d3934ff5185ce8938309c5b326dce73a83f0eb
VeselinBPavlov <veselin.b.pavlov@gmail.com>
2019-11-19 08:18:41 +0200

    Added annual report table and filter.

--- Src/Presentation/Finance.WebApp/ClientApp/src/components/AnnualReport/AnnualReport.js
+++ Src/Presentation/Finance.WebApp/ClientApp/src/components/AnnualReport/AnnualReport.js
@@ -1,4 +1,6 @@
 ﻿import React, { Component } from 'react';
+import { Filter } from './Filter/Filter';
+import { Table } from './Table/Table';
 import { getExpensesByYear } from '../../services/expense-service';
 import { getIncomesByYear } from '../../services/income-service';
 
@@ -9,7 +11,6 @@ export class AnnualReport extends Component {
             year: new Date().getFullYear(),
             expenses: [],
             incomes: [],
-            payToYourself: 0.1,
             totalExpenses: 0,
             totalIncomes: 0,
             loading: true
@@ -29,17 +30,27 @@ export class AnnualReport extends Component {
     }
 
     componentDidMount() {
-        getExpensesByYear(this.state.year)
-            .then(data => console.log(data))
-
-        getIncomesByYear(this.state.year)
-            .then(data => console.log(data))
+        this.populateData();
     }
 
     renderExpensesTable(expenses, totalExpenses, incomes, totalIncomes) {
         return (
             <div>
-                <h2>Monthly Report</h2>
+                <h2>Annual Report</h2>
+                <br />
+                <Filter
+                    yearChange={this.onYearChange}
+                    refresh={this.populateData}
+                    year={this.state.year}
+                />
+
+                <Table
+                    expenses={expenses}
+                    incomes={incomes}
+                    totalExpenses={totalExpenses}
+                    totalIncomes={totalIncomes}
+                    refresh={this.populateExpensesData}
+                />
             </div>
         );
     }
@@ -49,8 +60,31 @@ export class AnnualReport extends Component {
             year: event.target.value
         });
     }
+
+    populateExpensesData = () => {
+        getExpensesByYear(this.state.year)
+            .then(data => {
+                this.setState({
+                    expenses: data.expenseSums,
+                    totalExpenses: data.totals,
+                    loading: false
+                });
+            }).catch(err => console.log(err));
+    }
+
+    populateIncomesData = () => {
+        getIncomesByYear(this.state.year)
+            .then(data => {
+                this.setState({
+                    incomes: data.incomeSums,
+                    totalIncomes: data.totals,
+                    loading: false
+                });
+            }).catch(err => console.log(err));
+    }
     
     populateData = () => {
         this.populateExpensesData();
+        this.populateIncomesData();    
     }
 }
--- Src/Presentation/Finance.WebApp/ClientApp/src/components/AnnualReport/Filter/Filter.js
+++ Src/Presentation/Finance.WebApp/ClientApp/src/components/AnnualReport/Filter/Filter.js
@@ -7,12 +7,9 @@ export class Filter extends Component {
                 <br />
                 <div className="form-row">
                     <div className="col-md-2">
-                        <input className="form-control" name="Month" onChange={this.props.monthChange} placeholder={this.props.month} />
-                    </div>
-                    <div className="col-md-2">
                         <input className="form-control" name="Year" onChange={this.props.yearChange} placeholder={this.props.year} />
                     </div>
-                    <button className="btn btn-primary" onClick={this.props.refresh}>Get Cashflows</button>
+                    <button className="btn btn-primary" onClick={this.props.refresh}>Get Report</button>
                 </div>
                 <br />
             </Fragment>

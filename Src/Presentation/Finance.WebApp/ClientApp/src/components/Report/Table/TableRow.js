532c552fbe447dfb8023fd6d437b87781427eaa2
Veselin Pavlov <veselin.b.pavlov@gmail.com>
2019-11-20 09:17:52 +0200

    Initial Commit to develop branch.

--- Src/Presentation/Finance.WebApp/ClientApp/src/components/Report/Table/TableRow.js
+++ Src/Presentation/Finance.WebApp/ClientApp/src/components/Report/Table/TableRow.js
@@ -8,7 +8,7 @@ export class TableRow extends Component {
                 <td>{this.props.flow.typeId}</td>
                 <td>{this.props.flow.sum.toFixed(2)} lv.</td>
                 <td>{((this.props.flow.sum / this.props.totals) * 100).toFixed(2)}%</td>
-            </tr>
+            </tr> 
         )
     }
 }

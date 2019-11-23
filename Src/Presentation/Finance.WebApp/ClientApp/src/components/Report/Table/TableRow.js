1de0f9835569efa96c7419b892ccde823d1edda2
VeselinBPavlov <veselin.b.pavlov@gmail.com>
2019-11-23 21:18:02 +0200

    Fixed bugs saving percents and total pecent in monthly report.

--- Src/Presentation/Finance.WebApp/ClientApp/src/components/Report/Table/TableRow.js
+++ Src/Presentation/Finance.WebApp/ClientApp/src/components/Report/Table/TableRow.js
@@ -1,13 +1,16 @@
 ﻿import React, { Component } from 'react';
 
 export class TableRow extends Component {
+   
+
     render() {
+        let savingPer = this.props.flow.sum === 0 ? 0 : ((this.props.flow.sum / this.props.totals) * 100);
         return (
             <tr>
                 <td>{this.props.flow.name}</td>
                 <td>{this.props.flow.typeId}</td>
                 <td>{this.props.flow.sum.toFixed(2)} lv.</td>
-                <td>{((this.props.flow.sum / this.props.totals) * 100).toFixed(2)}%</td>
+                <td>{savingPer.toFixed(2)}%</td>
             </tr> 
         )
     }

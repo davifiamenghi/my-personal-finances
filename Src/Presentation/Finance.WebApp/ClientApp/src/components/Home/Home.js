40b7ddfa2b22d15d2cffd29d08604637301e2a67
Veselin Pavlov <veselin.b.pavlov@gmail.com>
2019-11-26 14:46:32 +0200

    Styled home page.

--- Src/Presentation/Finance.WebApp/ClientApp/src/components/Home/Home.js
+++ Src/Presentation/Finance.WebApp/ClientApp/src/components/Home/Home.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Carousel, Container, Row, Col, Button } from 'react-bootstrap';
+import { Info } from './Info';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> Finance. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={process.env.PUBLIC_URL + '/slide1.jpg'}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h1 className="slider">MY PERSONAL FINANCES</h1>
+                            <h4>Manage your finances easaly. Start today!</h4>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+                <Container>
+                    <Row>
+                        <Info
+                            title="Incomes"
+                            content="It is better to have more passive incomes. Thats why you have to invest."
+                            styleColor="#218838"
+                            buttonColor="success"
+                            link="/Incomes"
+                        />
+                        <Info
+                            title="Expenses"
+                            content="Expenses can be optimized for bigger monthly net cashflow."
+                            styleColor="#C82333"
+                            buttonColor="danger"
+                            link="/Expenses"
+                        />
+                        <Info
+                            title="Monthly Report"
+                            content="Attention to 'Pay to Yourself' row. Save your money before taxes."
+                            styleColor="#E0A800"
+                            buttonColor="warning"
+                            link="/Monthly-Report"
+                        />
+                        <Info
+                            title="Annual Report"
+                            content="Check your year savings. You can have more money for investments."
+                            styleColor="#0069D9"
+                            buttonColor="primary"
+                            link="/Annual-Report"
+                        />
+                    </Row>
+                </Container>
+            </Fragment>
+        );
+    }
 }
---
+++ Src/Presentation/Finance.WebApp/ClientApp/src/components/Layout/Footer.js
@@ -0,0 +1,76 @@
+﻿import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export class Footer extends Component {
+    static displayName = Footer.name;
+
+    render() {
+        return (
+            <footer>
+                <hr />
+                <Container>
+                    <Row>
+                        <Col md={4}>
+
+                            <h6 className="text-uppercase mb-4 font-weight-bold">My Personal Finances</h6>
+                            <p>My Personal Finances is web application wich priovides an easy management system for your income and expenses. Also with month and annual report you can take effective decision about your financial future.</p>
+
+                        </Col>
+                        <Col md={2}>
+
+                            <h6 className="text-uppercase mb-4 font-weight-bold">Futures</h6>
+                            <p>
+                                <Link to="/Incomes">Incomes</Link>
+                            </p>
+                            <p>
+                                <Link to="/Expenses">Expenses</Link>
+                            </p>
+                            <p>
+                                <Link to="/Monthly-Report">Monthly Report</Link>
+                            </p>
+                            <p>
+                                <Link to="/Annual-Report">Annual Report</Link>
+                            </p>
+
+                        </Col>
+                        <Col md={2}>
+
+                            <h6 className="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+                            <p>
+                                <a href="#!">Your Account</a>
+                            </p>
+                            <p>
+                                <a href="#!">Become an Affiliate</a>
+                            </p>
+                            <p>
+                                <a href="#!">Shipping Rates</a>
+                            </p>
+                            <p>
+                                <a href="#!">Help</a>
+                            </p>
+
+                        </Col>
+                        <Col md={4}>
+
+                            <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                            <p>
+                                <i className="fa fa-home mr-3"></i> New York, NY 10012, US</p>
+                            <p>
+                                <i className="fa fa-envelope mr-3"></i> info@gmail.com</p>
+                            <p>
+                                <i className="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+                            <p>
+                                <i className="fa fa-print mr-3"></i> + 01 234 567 89</p>
+
+                        </Col>
+                    </Row>
+                    <p className="text-center text-md-left">© 2018 Copyright:
+                         <Link to="/"><strong> my-personal-finances.com</strong></Link>
+                    </p>
+                </Container>
+            </footer>
+        );
+    }
+}
+
